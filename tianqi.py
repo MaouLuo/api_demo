8218f9d37272de53f8f74a1e6e7c2b047843f0f3
MaouLuo <mrsama@163.com>
2020-07-29 15:07:54 +0800

    多个小工具合并为tools库

--- tianqi.py
+++ tianqi.py
@@ -3,23 +3,12 @@
 import requests
 import json
 import re
-import configparser 
-from serverj import ServerJ
-
-# 读取配置文件函数
-# os.path.dirname(os.path.realpath(__file__)))获取当前路径，os.getcwd()获取有异常，仅能拿到1级目录
-def r_conf(item, path='D:\\code'+'\\config.ini', name='token'):
-    config = configparser.ConfigParser()
-    config.read(path, encoding='utf-8')
-    cont = config.get(item, name)
-    #print(cont)
-    return cont
+import tools
 
 def bad_wea(text):
     pattern = r'.*?[雨雪].*?'
     if re.search(pattern, text, flags=0):
         return True 
-    #print('none')
     return False
 
 def get_day(text):
@@ -30,34 +19,55 @@ def get_day(text):
 
 def today_bad_wea():
     title, cont = '', ''
-
+    bwea = False
     url = 'https://tianqiapi.com/api'    
     params = {
         'version': 'v1',
-        'appid': r_conf('tianqi', name='appid'),
-        'appsecret': r_conf('tianqi', name='appsecret')
+        'appid': tools.r_conf('tianqi', name='appid'),
+        'appsecret': tools.r_conf('tianqi', name='appsecret')
     }
 
     resp = requests.get(url, params=params)
-    jd = json.loads(resp.text)
-
-    day = get_day(jd['data'][0]['day'])
-    title = '{} {} {}'.format(day, jd['city'], jd['data'][0]['wea'])
-    cont += '当前{}，最低{}，最高{}'.format(jd['data'][0]['tem'], jd['data'][0]['tem2'], jd['data'][0]['tem1']) + '\r\n\r\n'
-
+    jd = json.loads(resp.text)    
     for w in jd['data'][0]['hours']:
         if bad_wea(w['wea']):
-            #print('{}，{}，{}'.format(w['day'], w['wea'], w['tem']))
             cont += '{}，{}，{}'.format(w['day'], w['wea'], w['tem']) + '\r\n\r\n'
-    if cont:
-        return title, cont
-    return False
+            bwea = True
+            #title = '{} {} {}'.format(day, jd['city'], jd['data'][0]['wea'])
+            #cont += '当前{}，最低{}，最高{}'.format(jd['data'][0]['tem'], jd['data'][0]['tem2'], jd['data'][0]['tem1']) + '\r\n\r\n'
+    if bwea:
+        day = get_day(jd['data'][0]['day'])
+        title = '{} {} {}'.format(day, jd['city'], jd['data'][0]['wea'])
+    else:
+        cont = False
+    return title, cont
+    
+# 用Nowapi实时天气接口，每1小时更新一次
+def get_cur_wea(weaid='南山'):
+    url = 'http://api.k780.com'
+    data = {
+        'weaid': weaid,
+        'app': 'weather.realtime',
+        'appkey': tools.r_conf('nowapi', name='appkey'),
+        'sign': tools.r_conf('nowapi', name='sign'),
+        'format': 'json'
+    }
+    resp = requests.post(url, data=data)
+    jd = json.loads(resp.text)
+
+    wtNm = jd['result']['realTime']['wtNm'] # 天气类型
+    wtTemp = jd['result']['realTime']['wtTemp'] # 温度
+
+    return {'wtNm':wtNm , 'wtTemp':wtTemp}
+
 
 def main():
     title, cont = today_bad_wea()
     if cont:
-        sj = ServerJ(title=title, cont=cont , token=r_conf('sj'), debug=False)
+        sj = tools.ServerJ(title=title, cont=cont , token=tools.r_conf('sj'), debug=True)
         sj.run()
+    else:
+        print('good wea')
 
 if __name__ == '__main__':
     main()
